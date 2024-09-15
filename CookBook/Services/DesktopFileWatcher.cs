fcd3114ee317cc0a2b669735770e925b5038c001
codebeautyprogramming <codebeautyprogramming@gmail.com>
2024-09-15 13:13:49 +0200

    Startup code for this chapter

---
+++ CookBook/Services/DesktopFileWatcher.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.Services
+{
+    public class DesktopFileWatcher
+    {
+        private static volatile DesktopFileWatcher _instance;
+        private static readonly object _lock = new object();
+        private BackgroundWorker _fileCheckWorker;
+        public static DesktopFileWatcher Instance
+        {
+            get 
+            {
+                if(_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new DesktopFileWatcher();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+        private DesktopFileWatcher() {
+            _fileCheckWorker = new BackgroundWorker();
+            _fileCheckWorker.DoWork += FileCheckWorker_DoWork;
+            _fileCheckWorker.RunWorkerAsync();
+        }
+
+        private void FileCheckWorker_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = Path.Combine(desktopPath, "ShoppingList.txt");
+
+            while (true)
+            {
+                bool fileExists = File.Exists(filePath);
+                //TODO: send notification about file status
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
